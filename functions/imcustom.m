4ed836f2ed2cc205c53a47b3595f24f07e068843
phuxel <phuxel@MW-PF57SS9E>
2026-03-28 20:02:01 -0400

    moved computer vision check from app to imcustom

--- functions/imcustom.m
+++ functions/imcustom.m
@@ -21,14 +21,16 @@ img = ind2rgb( gry, bone );  % <-- you can change the colormap here
 % img = isocolor( img, [0.5 1.3]/6 ); % <-- for example orange and greenish
 
 % You can delete the code below to remove the text
-% (or change the "msg" variable to add your own text)
-msg = "Modify 'imcustom.m' to apply your own effect!" + newline ...
-    + newline + ">> edit imcustom  % pause or close video first";
-
-% Add the text "msg" above to the the center of the image
-cen = size( img, [2 1] )/2; % [width,height]/2 = [xc,yc]
-img = insertText( img, cen, msg, AnchorPoint="center", ...
-    BoxOpacity=0.8, TextBoxColor="white", ...
-    FontSize=round(size(img,2)/25), FontColor=[0.7 0.5 0] );
+% (insertText require the Computer Vision Toolbox)
+hasCompVision = license( "test", "video_and_image_blockset" );
+if logical( hasCompVision )
+    % Add the text "msg" above to the the center of the image
+    msg = "Modify 'imcustom.m' to apply your own effect!" + newline ...
+        + newline + ">> edit imcustom  % pause or close video first";
+    cen = size( img, [2 1] )/2; % [width,height]/2 = [xc,yc]
+    img = insertText( img, cen, msg, AnchorPoint="center", ...
+        BoxOpacity=0.8, TextBoxColor="white", ...
+        FontSize=round(size(img,2)/25), FontColor=[0.7 0.5 0] );
+end
 
 end % end of function (you can leave this here)
